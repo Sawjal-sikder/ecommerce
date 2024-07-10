7c290d7b9765a0184c4aed1d6f193986587fa35d
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-07-10 17:26:47 +0600

    category add

--- main/products/urls.py
+++ main/products/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.products, name='products'),
     path('single_product/<slug:slug>', views.single_product, name='single_product'),
+    path('category_product/<slug:slug>', views.category_product, name='category_product'),
 ]
